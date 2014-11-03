e579a63ab380fe01694bbe9ca992c2c7547cd269
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2014-11-03 16:13:36 +0100

    Build pages

--- themes/norton/inc/page-builder-custom.php
+++ themes/norton/inc/page-builder-custom.php
@@ -5,3 +5,21 @@ function homePage_panels_row_styles($styles) {
 	return $styles;
 }
 add_filter('siteorigin_panels_row_styles', 'homePage_panels_row_styles');
+
+function myPages_panels_row_styles($styles) {
+	$styles['myPages'] = __('My Pages', 'sparkling');
+	return $styles;
+}
+add_filter('siteorigin_panels_row_styles', 'myPages_panels_row_styles');
+
+function myTestimonials_panels_row_styles($styles) {
+	$styles['myTestimonials'] = __('Testimonials', 'sparkling');
+	return $styles;
+}
+add_filter('siteorigin_panels_row_styles', 'myTestimonials_panels_row_styles');
+
+function myAwards_panels_row_styles($styles) {
+	$styles['myAwards'] = __('Awards', 'sparkling');
+	return $styles;
+}
+add_filter('siteorigin_panels_row_styles', 'myAwards_panels_row_styles');
