b46856560b6e570aa643620059a857d525897f7a
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2014-11-19 11:01:40 +0100

    Fix the sliders on firefox

--- themes/norton/inc/js/scripts.js
+++ themes/norton/inc/js/scripts.js
@@ -26,8 +26,8 @@ jQuery(document).ready(function() {
  * Function that adjusts the height of the rotating leader on the home page
  */
 function adjustLeaderHeight() {
-    var targetHeight = jQuery('.flexslider').css('height');
-    targetHeight = parseInt(targetHeight, 10);
-    jQuery('.flex-viewport').attr('style', 'height:' + targetHeight + 'px' + ' !important;' + ' overflow: hidden; position: relative');   
+    var targetHeight = jQuery('.flexslider').height();
+    jQuery('.flex-viewport').height(targetHeight);
+    jQuery('.flex-viewport').attr('style', 'min-height: ' + targetHeight + 'px' + ' !important;' + ' overflow: hidden; position: relative');   
     jQuery('.rotating-leader img').attr('style', 'height: ' + targetHeight / 4 + 'px' + ' !important');
 }
